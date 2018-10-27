a90c51f126efba0f686d12b427bc831e0de07957
Sergigb <sergi.garciab@e-campus.uab.cat>
2018-10-27 22:03:06 +0200

    now wavelet hashing works faster

--- feat_wavelet_hash.py
+++ feat_wavelet_hash.py
@@ -1,33 +1,41 @@
 import imagehash
+from PIL import Image
 
 
-def compare_wavelet_hashing(q_image, image):
+def get_hash(image):
     """
-    Computes the difference between 2 images using DWT (discrete wavelet transformation) based image hashes. The basis
-    function can be changed but the Haar wavelet works pretty well. The hash size is important too. These are some
-    results with different hash sizes:
+    Computes the hash of the image using DWT (discrete wavelet transformation) based image hashing. The basis function
+    can be changed but the Haar wavelet works pretty well. The hash size is important too.
+    :param image: image
+    :return: DWT based hash
+    """
+    return imagehash.whash(Image.fromarray(image), hash_size=16)
+
+
+def compare_wavelet_hashing(q_hash, im_hash):
+    """
+    Computes the difference between 2 hashes. These are some results with different hash sizes:
     4x4 hash size: 0.54
     8x8 hash size: 0.85
-    16x16 hash size: 0.92
+    16x16 hash size: 0.92 (~26 seconds)
     32x32 hash size: 0.92
     TODO: retrieve execution times
 
     We based our code on https://fullstackml.com/wavelet-image-hash-in-python-3504fdd282b5
-    :param q_image: query image
-    :param image: image
+    :param q_hash: hash of the query image
+    :param im_hash: hash of the image
     :return: difference between the two hashes
     """
-    q_hash = imagehash.whash(q_image, hash_size=16)
-    im_hash = imagehash.whash(image, hash_size=16)
 
     return (q_hash-im_hash)/len(q_hash.hash) ** 2
 
 
-def retrieve_best_results(q_image, database_imgs, K=10):
+def retrieve_best_results(q_image, database_imgs, database_hash, K=10):
 
     scores = []
+    q_hash = get_hash(q_image)
     for d_image, d_name in database_imgs:
-        scores.append((d_name, compare_wavelet_hashing(q_image, d_image)))
+        scores.append((d_name, compare_wavelet_hashing(q_hash, database_hash[d_name])))
 
     scores.sort(key=lambda s: s[1], reverse=False)
 
--- retrieve_img_2.py
+++ retrieve_img_2.py
@@ -1,12 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 
 
 from dataset_w1_gt import ground_truth
 from data_handler import Data
-from feat_wavelet_hash import retrieve_best_results
+from feat_wavelet_hash import retrieve_best_results, get_hash
+from feat_hsv_hst import retrieve_best_results as retrieve_best_results_hsv, get_hsv_hist
 
 
 def show_results(scores, museum_set, query_image, ground_truth_image):
@@ -40,11 +40,23 @@ def main():
     # test_ground_truth(ground_truth=ground_truth, museum_set=museum_set, query_set=query_set)
     eval_array = []
 
-    query_imgs = [[Image.fromarray(im), name] for im, name in data.query_imgs]
-    database_imgs = [[Image.fromarray(im), name] for im, name in data.database_imgs]
+    query_imgs = [[im, name] for im, name in data.query_imgs]
+    database_imgs = [[im, name] for im, name in data.database_imgs]
+    query_hist = [get_hsv_hist(im) for im, name in query_imgs]
+    database_hist = [get_hsv_hist(im) for im, name in database_imgs]
 
-    for q_image, q_name in query_imgs:
-        scores = retrieve_best_results(q_image, database_imgs)
+    database_hash = {}
+    for im, name in data.database_imgs:
+        database_hash[name] = get_hash(im)
+
+    for [q_image, q_name], q_hist in zip(query_imgs, query_hist):
+        scores = retrieve_best_results(q_image, database_imgs, database_hash)
+        # scores2 = retrieve_best_results_hsv(image_histH=q_hist,
+        #                                     database_imgs=database_imgs,
+        #                                     database_hist=database_hist)
+        #
+        # scores.sort(key=lambda s: s[0], reverse=False)
+        # scores2.sort(key=lambda s: s[0], reverse=False)
 
         eval = evaluation(predicted=[s[0] for s in scores], actual=[ground_truth[q_name]])
         print(eval)
